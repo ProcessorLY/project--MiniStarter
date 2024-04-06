7f72bde05a69f8031fa13d732fb7d147e1eb4ed3
Islam alshiki <eslam.alshiki94@gmail.com>
2024-04-06 10:00:49 +0200

    socity-test

---
+++ client/src/app/modules/socitybox/socitybox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-socitybox',
+  templateUrl: './socitybox.component.html',
+  styleUrls: ['./socitybox.component.css']
+})
+export class SocityboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/modules/socitybox/socitybox.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SocityboxComponent } from './socitybox.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SocityboxComponent]
+})
+export class SocityboxModule { }
